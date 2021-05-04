70db14cb338e09010d365b2898a2f24efdf8379b
Adrian Khor <adriank96@outlook.com>
2021-05-04 10:33:14 +0900

    Implemented simple string length retrieval message

---
+++ testobjc1/TestClass.m
@@ -0,0 +1,12 @@
+//
+//  TestClass.m
+//  testobjc1
+//
+//  Created by Adrian Khor on 03/05/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestClass : NSObject
+
+@end
--- testobjc1/ViewController.m
+++ testobjc1/ViewController.m
@@ -8,7 +8,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic, strong) NSString *name;
+@property (nonatomic) int age;
+@property (weak, nonatomic) IBOutlet UITextField *nameTextField;
+@property (weak, nonatomic) IBOutlet UILabel *nameInfoTextField;
 @end
 
 @implementation ViewController
@@ -16,7 +19,54 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    // Initializes the 'name' property and give it an initial value
+    //self.name = [[NSString alloc]initWithString:@"Hello "];
+    // The "modern way"
+    self.name = @"Hello ";
+    self.age = 42;
+    
+    // Calls the NSString function stringByAppendingString against the name
+    // property of the ViewController class
+    [self.name stringByAppendingString:@"Test"];
+    
+    // Calls the foo function and pass the appropriate arguments.
+    [self foo: @"Some string" :42];
+    
+    // Logs a value to the console
+    // %@ means NSString, %s means char*, etc
+    NSLog(@"Name is: %@", self.name);
+    
+    //
+    [self.nameTextField setText:self.name];
 }
 
 
+- (int)foo:(NSString*) bar : (int) baz {
+    return baz;
+}
+
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    // Informs the view that editing is done - and dismisses the keyboard
+    // if it is open.
+    [self.view endEditing:YES];
+}
+
+-(void)setInfoMessage:(NSString*) message {
+    [self.nameInfoTextField setText:[NSString stringWithFormat:@"%@", message]];
+}
+                                     
+- (IBAction)buttonClicked:(id)sender {
+    // Returns NO if length <= 0
+    BOOL isNameEmpty = ![self.nameTextField text].length;
+    
+    // Show an error message if name is empty
+    if(isNameEmpty) {
+        [self setInfoMessage:@"Name is empty!"];
+        return;
+    }
+    
+    // Sets the label text to include the length of the name
+    [self setInfoMessage:[NSString stringWithFormat:@"Name length: %ld",[self.nameTextField text].length]];
+}
 @end
